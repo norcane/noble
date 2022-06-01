736516d6369a47f994564b0baa9aab6fabf2e5bb
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2022-06-01 16:20:54 +0200

    Dummy REST controller skeleton

---
+++ src/main/java/com/norcane/noble/controller/NobleController.java
@@ -0,0 +1,13 @@
+package com.norcane.noble.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NobleController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello from noble controller!";
+    }
+}
