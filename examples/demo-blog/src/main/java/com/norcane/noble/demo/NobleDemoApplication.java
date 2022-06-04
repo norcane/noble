72b1d0995eaea061c69bb92fb7cb373edb783437
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2022-06-04 16:35:15 +0200

    Convert into multi-module project

---
+++ examples/demo-blog/src/main/java/com/norcane/noble/demo/NobleDemoApplication.java
@@ -0,0 +1,14 @@
+package com.norcane.noble.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.norcane.noble")
+public class NobleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NobleDemoApplication.class, args);
+    }
+}
